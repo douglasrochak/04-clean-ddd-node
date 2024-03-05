d71e5d37090c437c99b61a4d13d6fa03cfde4182
Douglas R <douglas.srochak@gmail.com>
2024-03-05 03:50:12 -0300

    feat: Add entity exports

---
+++ src/domain/entities/index.ts
@@ -0,0 +1,6 @@
+import Instructor from './instructor';
+import Answer from './answer';
+import Question from './question';
+import Student from './student';
+
+export { Instructor, Answer, Question, Student };
