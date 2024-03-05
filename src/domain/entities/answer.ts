29967be866b6489e5099193207a4f98e1e315a17
Douglas R <douglas.srochak@gmail.com>
2024-03-05 03:50:05 -0300

    feat: Add Answer entity class

---
+++ src/domain/entities/answer.ts
@@ -0,0 +1,60 @@
+import { Entity, UniqueEntityID } from '@/core/entities';
+import { Optional } from '@/core/types';
+
+interface AnswerProps {
+  content: string;
+  authorId: UniqueEntityID;
+  questionId: UniqueEntityID;
+  createdAt: Date;
+  updatedAt?: Date;
+}
+
+export default class Answer extends Entity<AnswerProps> {
+  static create(
+    props: Optional<AnswerProps, 'createdAt'>,
+    id?: UniqueEntityID
+  ) {
+    const answer = new Answer(
+      {
+        ...props,
+        createdAt: new Date(),
+      },
+      id
+    );
+
+    return answer;
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date();
+  }
+
+  set content(content: string) {
+    this.props.content = content;
+    this.touch();
+  }
+
+  get excerpt() {
+    return this.content.substring(0, 120).trimEnd().concat('...');
+  }
+
+  get content() {
+    return this.props.content;
+  }
+
+  get authorId() {
+    return this.props.authorId;
+  }
+
+  get questionId() {
+    return this.props.questionId;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
+}
