3022f807772bc316413128854b0c38a38f74b6b4
Douglas R <douglas.srochak@gmail.com>
2024-03-05 03:50:30 -0300

    feat: Add answers repository and answer question use case

---
+++ src/domain/use-cases/answer-question.ts
@@ -0,0 +1,29 @@
+import UniqueEntityID from '../../core/entities/unique-entity-id';
+import { Answer, Student } from '../entities';
+import { AnswersRepository } from '../repositories/answers-repository';
+
+interface AnswerQuestionUseCaseRequest {
+  instructorId: string;
+  questionId: string;
+  content: string;
+}
+
+export default class AnswerQuestionUseCase {
+  constructor(private repo: AnswersRepository) {}
+
+  async execute({
+    instructorId,
+    questionId,
+    content,
+  }: AnswerQuestionUseCaseRequest) {
+    const answer = Answer.create({
+      content,
+      authorId: new UniqueEntityID(instructorId),
+      questionId: new UniqueEntityID(questionId),
+    });
+
+    await this.repo.create(answer);
+
+    return answer;
+  }
+}
