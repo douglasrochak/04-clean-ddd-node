fa062ee2c41c5ee4995f44d1c586430c3af0fd15
Douglas R <douglas.srochak@gmail.com>
2024-03-05 03:49:37 -0300

    feat:  Add core types and question entity

---
+++ src/domain/entities/question.ts
@@ -0,0 +1,84 @@
+import { Optional } from '@/core/types';
+import { Slug } from '../shared';
+import { Entity, UniqueEntityID } from '@/core/entities';
+import dayjs from 'dayjs';
+
+interface QuestionProps {
+  title: string;
+  slug: Slug;
+  content: string;
+  authorId: UniqueEntityID;
+  bestAnswerId?: UniqueEntityID;
+  createdAt: Date;
+  updatedAt?: Date;
+}
+
+export default class Question extends Entity<QuestionProps> {
+  static create(
+    props: Optional<QuestionProps, 'createdAt' | 'slug'>,
+    id?: UniqueEntityID
+  ) {
+    const question = new Question(
+      {
+        ...props,
+        slug: props.slug ?? Slug.createFromText(props.title),
+        createdAt: new Date(),
+      },
+      id
+    );
+
+    return question;
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date();
+  }
+
+  set title(title: string) {
+    this.props.title = title;
+    this.props.slug = Slug.createFromText(title);
+    this.touch();
+  }
+
+  set content(content: string) {
+    this.props.content = content;
+    this.touch();
+  }
+
+  set bestAnswerId(answerId: UniqueEntityID | undefined) {
+    this.props.bestAnswerId = answerId;
+    this.touch();
+  }
+
+  get isNew() {
+    return dayjs().diff(this.createdAt, 'days') <= 3;
+  }
+
+  get title() {
+    return this.props.title;
+  }
+
+  get slug() {
+    return this.props.slug;
+  }
+
+  get content() {
+    return this.props.content;
+  }
+
+  get authorId() {
+    return this.props.authorId;
+  }
+
+  get bestAnswerId() {
+    return this.props.bestAnswerId;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
+}
