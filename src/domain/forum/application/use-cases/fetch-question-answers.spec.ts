3c60adf49e1e16b559f0a1b5b81cf9929eafcbd8
Douglas R <douglas.srochak@gmail.com>
2024-03-12 20:25:27 -0300

    feat: Add FetchQuestionAnswersUseCase and its unit tests

---
+++ src/domain/forum/application/use-cases/fetch-question-answers.spec.ts
@@ -0,0 +1,42 @@
+import { makeAnswer } from "test/factories/make-answer"
+import FetchQuestionAnswersUseCase from "./fetch-question-answers"
+import { MemoryAnswersRepo } from "test/memory-answers-repo"
+import { UniqueEntityID } from "@/core/entities"
+
+describe("Fetch Question Answers Use Case", () => {
+  let repo: MemoryAnswersRepo
+  let sut: FetchQuestionAnswersUseCase
+
+  beforeEach(() => {
+    repo = new MemoryAnswersRepo()
+    sut = new FetchQuestionAnswersUseCase(repo)
+  })
+
+  it("Should be able to fetch question answers", async () => {
+    const questionId = new UniqueEntityID("question-1")
+    await repo.create(makeAnswer({ questionId }))
+    await repo.create(makeAnswer({ questionId }))
+    await repo.create(makeAnswer({ questionId }))
+
+    const { answers } = await sut.execute({
+      questionId: questionId.toString(),
+      page: 1,
+    })
+
+    expect(answers).toHaveLength(3)
+  })
+
+  it("Should be able to fetch paginated question answers", async () => {
+    const questionId = new UniqueEntityID("question-1")
+    for (let i = 0; i < 22; i++) {
+      await repo.create(makeAnswer({ questionId }))
+    }
+
+    const { answers } = await sut.execute({
+      questionId: questionId.toString(),
+      page: 2,
+    })
+
+    expect(answers).toHaveLength(2)
+  })
+})
