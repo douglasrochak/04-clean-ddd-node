bb7cdc47f494ac094ef040ec6f6d077859334435
Douglas R <douglas.srochak@gmail.com>
2024-03-12 20:23:18 -0300

    feat: Add pagination and CRUD operations to memory repositories

--- test/memory-answers-repo.ts
+++ test/memory-answers-repo.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from "@/core/repositories/pagination-params"
 import { AnswersRepository } from "@/domain/forum/application/repositories/answers-repository"
 import { Answer } from "@/domain/forum/enterprise/entities"
 
@@ -6,5 +7,34 @@ export class MemoryAnswersRepo implements AnswersRepository {
 
   async create(answer: Answer) {
     this.items.push(answer)
+  }
+
+  async save(answer: Answer): Promise<void> {
+    const answerIndex = this.items.findIndex((item) => item.id === answer.id)
+
+    this.items[answerIndex] = answer
+  }
+
+  async delete(answer: Answer): Promise<void> {
+    const answerIndex = this.items.findIndex((item) => item.id === answer.id)
+
+    this.items.splice(answerIndex, 1)
+  }
+
+  async findById(id: string): Promise<Answer | null> {
+    const answer = this.items.find((item) => item.id.toString() === id)
+    if (!answer) return null
+    return answer
+  }
+
+  async findManyByQuestionId(
+    questionId: string,
+    { page }: PaginationParams
+  ): Promise<Answer[]> {
+    const answers = this.items
+      .filter((item) => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20)
+
+    return answers
   }
 }
--- test/memory-questions-repo.ts
+++ test/memory-questions-repo.ts
@@ -1,8 +1,45 @@
+import { PaginationParams } from "@/core/repositories/pagination-params"
 import { QuestionsRepository } from "@/domain/forum/application/repositories/questions-repository"
 import { Question } from "@/domain/forum/enterprise/entities"
 
 export class MemoryQuestionsRepo implements QuestionsRepository {
   public items: Question[] = []
+
+  async findManyRecent({ page }: PaginationParams): Promise<Question[]> {
+    const questions = this.items
+      .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+      .slice((page - 1) * 20, page * 20)
+
+    return questions
+  }
+
+  async delete(question: Question): Promise<void> {
+    const questionIndex = this.items.findIndex(
+      (item) => item.id === question.id
+    )
+
+    this.items.splice(questionIndex, 1)
+  }
+
+  async save(question: Question): Promise<void> {
+    const questionIndex = this.items.findIndex(
+      (item) => item.id === question.id
+    )
+
+    this.items[questionIndex] = question
+  }
+
+  async findById(id: string): Promise<Question | null> {
+    const question = this.items.find((item) => item.id.toString() === id)
+    if (!question) return null
+    return question
+  }
+
+  async findBySlug(slug: string): Promise<Question | null> {
+    const question = this.items.find((item) => item.slug.value === slug)
+    if (!question) return null
+    return question
+  }
 
   async create(question: Question) {
     this.items.push(question)
