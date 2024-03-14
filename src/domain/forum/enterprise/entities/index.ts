b35fbdc4ccf5fa991cb8ce4ed05bcbacbfcce2bd
Douglas R <douglas.srochak@gmail.com>
2024-03-14 01:58:16 -0300

    chore: Add AnswerComment and QuestionComment entities

--- src/domain/forum/enterprise/entities/index.ts
+++ src/domain/forum/enterprise/entities/index.ts
@@ -2,5 +2,7 @@ import Instructor from "./instructor"
 import Answer from "./answer"
 import Question from "./question"
 import Student from "./student"
+import AnswerComment from "./answer-comment"
+import QuestionComment from "./question-comment"
 
-export { Instructor, Answer, Question, Student }
+export { Instructor, Answer, Question, Student, AnswerComment, QuestionComment }
