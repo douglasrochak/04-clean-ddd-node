7ad8cbb5cc3c2f31c6e88834265efeea26212110
Douglas R <douglas.srochak@gmail.com>
2024-03-14 14:44:11 -0300

    test: refactor create question use case tests

--- src/domain/forum/application/use-cases/create-question.spec.ts
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -1,5 +1,6 @@
 import { MemoryQuestionsRepo } from "test/memory-questions-repo"
 import CreateQuestionUseCase from "./create-question"
+import { UniqueEntityID } from "@/core/entities"
 
 describe("Create Question", () => {
   let repo: MemoryQuestionsRepo
@@ -15,9 +16,23 @@ describe("Create Question", () => {
       authorId: "fake-author-id",
       title: "Question 1",
       content: "Question content",
+      attachmentsIds: ["1", "2"],
     })
 
     expect(result.isRight()).toEqual(true)
     expect(repo.items[0].id).toEqual(result.value?.question.id)
+    expect(repo.items[0].attachments.currentItems).toHaveLength(2)
+    expect(repo.items[0].attachments.currentItems).toEqual([
+      expect.objectContaining({
+        questionId: result.value?.question.id,
+      }),
+      expect.objectContaining({
+        questionId: result.value?.question.id,
+      }),
+    ])
+    expect(repo.items[0].attachments.currentItems).toEqual([
+      expect.objectContaining({ attachmentId: new UniqueEntityID("1") }),
+      expect.objectContaining({ attachmentId: new UniqueEntityID("2") }),
+    ])
   })
 })
