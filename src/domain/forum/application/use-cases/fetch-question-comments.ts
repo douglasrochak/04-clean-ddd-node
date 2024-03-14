e8969c6a516256575e7d74f350d1ac52a547ac31
Douglas R <douglas.srochak@gmail.com>
2024-03-14 04:31:18 -0300

    feat: Add FetchQuestionCommentsUseCase to fetch comments for a question

---
+++ src/domain/forum/application/use-cases/fetch-question-comments.ts
@@ -0,0 +1,26 @@
+import { QuestionComment } from "../../enterprise/entities"
+import { QuestionCommentsRepository } from "../repositories/question-comments-repository"
+
+interface FetchQuestionCommentsUseCaseRequest {
+  questionId: string
+  page: number
+}
+
+interface FetchQuestionCommentsUseCaseResponse {
+  questionComments: QuestionComment[]
+}
+
+export default class FetchQuestionCommentsUseCase {
+  constructor(private repo: QuestionCommentsRepository) {}
+
+  async execute({
+    questionId,
+    page,
+  }: FetchQuestionCommentsUseCaseRequest): Promise<FetchQuestionCommentsUseCaseResponse> {
+    const questionComments = await this.repo.findManyByQuestionId(questionId, {
+      page,
+    })
+
+    return { questionComments }
+  }
+}
