b41e1587b4c4536d5247a9e353f0eb944b7d223d
Douglas R <douglas.srochak@gmail.com>
2024-03-14 04:18:49 -0300

    feat: Add AnswerCommentsRepository interface and fix file name

--- src/domain/forum/application/repositories/answer-comments-repository.ts
+++ src/domain/forum/application/repositories/answer-comments-repository.ts
@@ -2,4 +2,6 @@ import { AnswerComment } from "../../enterprise/entities"
 
 export interface AnswerCommentsRepository {
   create(answerComment: AnswerComment): Promise<void>
+  findById(id: string): Promise<AnswerComment | null>
+  delete(questionComment: AnswerComment): Promise<void>
 }
