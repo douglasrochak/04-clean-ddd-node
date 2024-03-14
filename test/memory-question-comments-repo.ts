2bf9795b1eb59d16ec773cd0960c19a19bb6ae43
Douglas R <douglas.srochak@gmail.com>
2024-03-14 03:20:18 -0300

    test: Add makeQuestion factory function and implement findById and delete methods in MemoryQuestionCommentsRepo

--- test/memory-question-comments-repo.ts
+++ test/memory-question-comments-repo.ts
@@ -7,4 +7,18 @@ export class MemoryQuestionCommentsRepo implements QuestionCommentsRepository {
   async create(questionComment: QuestionComment) {
     this.items.push(questionComment)
   }
+
+  async findById(id: string): Promise<QuestionComment | null> {
+    const questionComment = this.items.find((item) => item.id.toString() === id)
+    if (!questionComment) return null
+    return questionComment
+  }
+
+  async delete(questionComment: QuestionComment): Promise<void> {
+    const questionIndex = this.items.findIndex(
+      (item) => item.id === questionComment.id
+    )
+
+    this.items.splice(questionIndex, 1)
+  }
 }
