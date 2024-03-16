4102c9dd1bc5625697cb5bda9d84757dea97e857
Douglas R <douglas.srochak@gmail.com>
2024-03-16 08:01:52 -0300

    test: Add DomainEvents import and dispatch events for create and save operations

--- test/memory-answers-repository.ts
+++ test/memory-answers-repository.ts
@@ -1,3 +1,4 @@
+import { DomainEvents } from "@/core/events"
 import { PaginationParams } from "@/core/repositories/pagination-params"
 import { AnswerAttachmentsRepository } from "@/domain/forum/application/repositories/answer-attachments-repository"
 import { AnswersRepository } from "@/domain/forum/application/repositories/answers-repository"
@@ -12,12 +13,16 @@ export class MemoryAnswersRepo implements AnswersRepository {
 
   async create(answer: Answer) {
     this.items.push(answer)
+
+    DomainEvents.dispatchEventsForAggregate(answer.id)
   }
 
   async save(answer: Answer): Promise<void> {
     const answerIndex = this.items.findIndex((item) => item.id === answer.id)
 
     this.items[answerIndex] = answer
+
+    DomainEvents.dispatchEventsForAggregate(answer.id)
   }
 
   async delete(answer: Answer): Promise<void> {
